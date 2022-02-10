9b9109f6cf939570942bfdf2fc67f33b0dc85cbe
Eupos <eupos@mail.ru>
2022-02-10 19:51:57 +0300

    Создал класс для связки Layout

---
+++ app/src/main/java/ru/geekbrains/eupos/android1/practice5/BaseActivity.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.eupos.android1.practice5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        recreate();
+    }
+}
+
